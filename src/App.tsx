754c080a77866332952233ff90ee52bcc581fb43
Rayck Sousa <80211838+Raycksousa@users.noreply.github.com>
2022-03-16 18:34:02 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import {ListItem} from './components/listItem';
+import {AddArea} from './components/AddArea';
+
+
+const App = () => {
+  const[list, setList] = useState<Item[]>([
+    {id:1, name:'comprar um pao', done:false},
+    {id:2, name:'comprar um bolo', done:false}
+  ]);
+
+  const handleAddTask = (taskName:string) => {
+     let newList = [...list];
+     newList.push({
+       id: list.length + 1,
+       name: taskName,
+       done:false
+     });
+     setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
+
+
+   return (
+       <C.Container>
+         <C.Area>
+           <C.Header>Lista de tarefas</C.Header>
+          
+          {/* Area de adiconar nova tarefa */}
+
+          <AddArea  onEnter={handleAddTask}/> 
+
+
+          {/* Lista de tarefas */}
+
+          {list.map((item, index)=>(
+            <ListItem 
+             key={index}
+             item={item}
+             onChange={handleTaskChange} 
+              />
+          ))}
+
+         </C.Area>
+       </C.Container>
+   );
+
+}
+
+export default App;
